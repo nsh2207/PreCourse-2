f44e62d668b97e2922083db776632c3583c336a4
nsh2207 <sn4165@nyu.edu>
2025-01-02 18:59:23 -0500

    Implemented iterative version of quicksort using stacks and pairs Exercise_5

--- Exercise_5.cpp
+++ Exercise_5.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h> 
+#include <iostream>
+#include <stack>
+#include <utility>
 using namespace std; 
   
 // A utility function to swap two elements 
@@ -10,9 +13,20 @@ void swap(int* a, int* b)
 } 
   
 /* This function is same in both iterative and recursive*/
-int partition(int arr[], int l, int h) 
+int partition(int arr[], int low, int high) 
 { 
     //Do the comparison and swapping here 
+     //Your Code here 
+    int i = low, j = low;
+    while(j<=high-1){
+        if(arr[j]<=arr[high]){
+            swap(&arr[i],&arr[j]);
+            i++;
+        }
+        j++;
+    }
+    swap(&arr[i],&arr[high]);
+    return i;
 } 
   
 /* A[] --> Array to be sorted,  
@@ -21,6 +35,26 @@ h --> Ending index */
 void quickSortIterative(int arr[], int l, int h) 
 { 
     //Try to think that how you can use stack here to remove recursion.
+    stack<pair<int,int> >params;
+    pair<int,int>curr;
+    curr.first = l;
+    curr.second = h;
+    params.push(curr);
+    while(!params.empty()){
+        pair<int,int>curr = params.top();
+        params.pop();
+        int low = curr.first, high = curr.second;
+        if(low>=high) continue;
+        int low_part = partition(arr,low,high);
+        pair<int,int>left_curr;
+        left_curr.first = low;
+        left_curr.second = low_part-1;
+        pair<int,int> right_curr;
+        right_curr.first = low_part+1;
+        right_curr.second = high;
+        params.push(left_curr);
+        params.push(right_curr);
+    }
 } 
   
 // A utility function to print contents of arr 
